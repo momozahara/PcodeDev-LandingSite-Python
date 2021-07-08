ff65507858a69a5a280508fec49a6a2dc3c8f6ed
momozahara <momozahara@hotmail.com>
2021-07-09 01:10:45 +0700

    Update Watch

---
+++ static/main/video/js/watch.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('<iframe />', {
+        id: "PP",
+        src: "https://www.youtube.com/embed/" + vid,
+        title: "YouTube video player",
+        frameborder: 0,
+        allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+        allowfullscreen: true
+    }).appendTo('#embed');
+    $.getJSON("https://api.pcode.dev/get/v/data/" + vid, function (data) {
+        desc_uploader.attr("href", "https://www.youtube.com/channel/" + data.items.channelId);
+        desc_uploader.text(data.items.channelTitle);
+        $(document).prop("title", document.title.replace("{{ vid }}", data.items.title));
+    });
+});
